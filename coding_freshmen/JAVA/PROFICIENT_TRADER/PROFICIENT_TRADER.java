0b756363737035a3cd55a80da6dfa01e50928ac3
Soumyaranjan <84384924+soumyaranjan-panda@users.noreply.github.com>
2022-10-02 13:24:07 +0530

    Update PROFICIENT_TRADER.java

--- coding_freshmen/JAVA/PROFICIENT_TRADER/PROFICIENT_TRADER.java
+++ coding_freshmen/JAVA/PROFICIENT_TRADER/PROFICIENT_TRADER.java
@@ -1,1 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
 
+        int[] arr = {7,1,5,3,6,4};
+        System.out.println(maxProfit(arr));
+    }
+    static int maxProfit(int[] arr){
+        int max = 0, profit = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            profit = arr[i+1] - arr[i];
+            if (profit > max){
+                max = profit;
+            }
+        }
+        return max;
+    }
+}
