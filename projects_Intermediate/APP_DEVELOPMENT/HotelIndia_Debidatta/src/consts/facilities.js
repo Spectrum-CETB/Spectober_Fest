c6b3d61ff320a4e86e8872b9a80742e105a03634
debidattasuryaprakash <suryaprakash.d20@gmail.com>
2022-10-03 15:50:14 +0000

    HotelIndia Application

---
+++ projects_Intermediate/APP_DEVELOPMENT/HotelIndia_Debidatta/src/consts/facilities.js
@@ -0,0 +1,31 @@
+const facilities = [
+  {
+    id: "1",
+    name: "Nirmaan Hotel",
+    location: "Bhubaneswar, Odisha",
+    price: "120",
+    image: require("../assets/hotel1.jpg"),
+    details: "Lorem lets get started lemmmsss gooooo",
+    about: "Lorem Ipsum",
+    facilities:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    id: "2",
+    name: "Sit Hotel",
+    location: "Bhubaneswar, Odisha",
+    price: "120",
+    image: require("../assets/hotel1.jpg"),
+    details: "Lorem lets get started lemmmsss gooooo",
+  },
+  {
+    id: "3",
+    name: "OUTR Hotel",
+    location: "Bhubaneswar, Odisha",
+    price: "120",
+    image: require("../assets/hotel1.jpg"),
+    details: "Lorem lets get started lemmmsss gooooo",
+  },
+];
+
+export default facilities;
