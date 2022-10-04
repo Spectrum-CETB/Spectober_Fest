467f5b5006e9b43075b938a2a891912dd8b9c96d
ABUZANAZAD <114815467+ABUZANAZAD@users.noreply.github.com>
2022-10-04 13:17:56 +0530

    Changes in the code

--- coding_freshmen/JAVA/ABUZANAZAD/SORT_01.java
+++ coding_freshmen/JAVA/ABUZANAZAD/SORT_01.java
@@ -1,19 +1,11 @@
 import java.util.Arrays;
-import java.util.Scanner;
-
 class sort {
-
-
-    public static void main(String[] args)
+   public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        int i,n;
-        n= sc.nextInt();
-        int[] arr = new int[n];
-        for(i=0;i<n;i++)
-            arr[i]=sc.nextInt();
+        int[] arr ={0,1,0,1,1,0,0};
+
         Arrays.sort(arr);
 
-        System.out.println("Modified arr[] : %s", Arrays.toString(arr));
+        System.out.printf("Modified arr[] : %s", Arrays.toString(arr));
     }
 }
