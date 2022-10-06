3fade3f24cf1021562b0c13027a716c5b6a409f4
priyanka1971 <104821422+priyanka1971@users.noreply.github.com>
2022-10-06 08:31:38 +0530

    sort _01 question commited
    the question was about sorting 0 and 1 so that the all 0 of the array will be on the left side of the new array and all 1 will be on the righjt side of the new attay

--- coding_freshmen/C++/example_code.cpp
+++ coding_freshmen/C++/example_code.cpp
@@ -1,1 +1,38 @@
-
+/*sort_01*/
+#include<iostream>
+using namespace std;
+void seg 0 and 1(int arr[100],int n)
+{
+	int count=0;
+	for(int i=0;i<n;i++)
+	{
+		if(arr[i]==0)
+		count++;
+	}
+	for(int i=0;i<count;i++)
+	{
+		arr[i]=0;
+	}
+	for(int i=count;i<n;i++)
+	{
+		arr[i]=1;
+	}
+}
+void printarray(int arr[100],int n)
+{
+	cout<<"new array is";
+	for(int i=0;i<n;i++)
+	cout<<arr[i]<<" ";
+}
+int main()
+{
+	int arr[100],n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	seg 0 and 1(int arr[n],int n);
+	printarray(arr,n);
+	return 0;
+}
