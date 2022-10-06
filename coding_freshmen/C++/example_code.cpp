05b66b873478dd84e3347d7aaba009afb4793995
priyanka1971 <104821422+priyanka1971@users.noreply.github.com>
2022-10-06 09:02:37 +0530

    majority_element
    this is to find majority element in a array

--- coding_freshmen/C++/example_code.cpp
+++ coding_freshmen/C++/example_code.cpp
@@ -1,38 +1,38 @@
-/*sort_01*/
+/*majority element*/
 #include<iostream>
 using namespace std;
-void seg 0 and 1(int arr[100],int n)
-{
-	int count=0;
+void majorityele(int arr[],int n)
+{ 
+	int mcount =0;
+	int index=-1;
 	for(int i=0;i<n;i++)
 	{
-		if(arr[i]==0)
-		count++;
-	}
-	for(int i=0;i<count;i++)
-	{
-		arr[i]=0;
-	}
-	for(int i=count;i<n;i++)
-	{
-		arr[i]=1;
-	}
+            int count =0;
+		for(j=0;j<n;j++)
+		{
+			if(arr[i]==arr[j])
+				count++;
+		}
+	
+if(count>mcount)
+{
+	mcount=count;
+	index =i;
 }
-void printarray(int arr[100],int n)
-{
-	cout<<"new array is";
-	for(int i=0;i<n;i++)
-	cout<<arr[i]<<" ";
+}
+if(mcount>n/2)
+	cout<<arr[index]<<endl;
+	else
+		cout<<" majority element not found"<<endl;
 }
 int main()
 {
-	int arr[100],n;
+	int arr[100],n,i;
 	cin>>n;
-	for(int i=0;i<n;i++)
+	for(i=0;i<n;i++)
 	{
 		cin>>arr[i];
 	}
-	seg 0 and 1(int arr[n],int n);
-	printarray(arr,n);
+	majorityele(arr[],n);
 	return 0;
 }
