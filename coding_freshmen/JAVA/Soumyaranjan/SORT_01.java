7556dada68cc1b53d4f5fa607f23878ebf5602c9
Soumyaranjan <84384924+soumyaranjan-panda@users.noreply.github.com>
2022-10-10 17:34:22 +0530

    Create SORT_01.java

---
+++ coding_freshmen/JAVA/Soumyaranjan/SORT_01.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class sort {
+    public static void main(String[] args)
+    {
+        int[] arr = new int[]{0, 1, 0, 1, 1, 0, 0};
+        Arrays.sort(arr);
+        System.out.printf(Arrays.toString(arr));
+    }
+}
