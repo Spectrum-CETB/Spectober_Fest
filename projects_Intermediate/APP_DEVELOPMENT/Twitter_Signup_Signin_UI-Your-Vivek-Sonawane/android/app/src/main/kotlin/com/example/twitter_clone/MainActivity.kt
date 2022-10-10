aa3a97ab9009cff4d17546b61f0a9a3566b6b19f
viveeeeeek <viveksonawane600@gmail.com>
2022-10-10 18:30:11 +0530

    Fixed

--- projects_Intermediate/APP_DEVELOPMENT/Twitter_Signup_Signin_UI-Your-Vivek-Sonawane/android/app/src/main/kotlin/com/example/twitter_clone/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.twitter_clone
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- projects_Intermediate/APP_DEVELOPMENT/Twitter_Signup_Signin_UI-Your-Vivek-Sonawane/lib/meta/widgets/divider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:twitter_clone/app/shared/colors.dart';
-
-class CustomDivider extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Divider(color: textColor, thickness: 0.2);
-  }
-}
