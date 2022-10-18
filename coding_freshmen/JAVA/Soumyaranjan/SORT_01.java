5765d0f31ce1329bfd9a20f72456552583de82c1
Soumyaranjan <84384924+soumyaranjan-panda@users.noreply.github.com>
2022-10-18 21:46:42 +0530

    Update SORT_01.java

--- coding_freshmen/JAVA/Soumyaranjan/SORT_01.java
+++ coding_freshmen/JAVA/Soumyaranjan/SORT_01.java
@@ -1,16 +1,18 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-class sort {
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        int l = sc.nextInt();
-        int[] arr = new int[l];
-        for (int i = 0; i < l; i++) {
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+    int l = sc.nextInt ();
+    int[] arr = new int[l];
+    for (int i = 0; i < l; i++)
+      {
+	arr[i] = sc.nextInt ();
+      }
+    Arrays.sort (arr);
+    System.out.println (Arrays.toString (arr));
+  }
 }
